7acb8b477b700a930fee85a4e007602c98c41f74
Radharaman Gurjar <156350993+Radharamangurjar315@users.noreply.github.com>
2025-04-21 16:00:16 +0530

    Delete server/keys.js

--- server/keys.js
+++
@@ -1,5 +0,0 @@
-// Used to store the MongoDB URI
-module.exports = {
-    MONGOURI :"mongodb+srv://gurjarradharaman03:VPxrgfsHLK9CLKtl@minorproject.ltd9k.mongodb.net/?retryWrites=true&w=majority&appName=minorProject",
-    JWT_SECRET : "ranchhoddasphunsukhwangdu"
-}

9aaa38c3ea1d8962e7cdfe60af686acf207b0734
Radharaman Gurjar <gurjar.radharaman03@gmail.com>
2025-04-21 15:39:55 +0530

    removed unnecessary files

--- server/controllers/notificationController.js
+++
@@ -1,9 +0,0 @@
-const createNotification = require("../utils/createNotification");
-
-// Inside your event/poll/notice/complaint creation controller
-await createNotification({
-    message: `${req.user.name} posted a new ${type}`,
-    type: "poll",
-    societyId: req.user.societyId,
-    createdBy: req.user._id
-});

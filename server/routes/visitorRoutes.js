ffe7741bbe797615016daf57f03597dafeff4dd7
Radharaman Gurjar <gurjar.radharaman03@gmail.com>
2025-04-29 13:05:22 +0530

    fixed bugs

--- server/routes/visitorRoutes.js
+++ server/routes/visitorRoutes.js
@@ -4,7 +4,7 @@ const visitorController = require('../controllers/visitorController');
 const requiredLogin = require('../middlewares/requiredLogin');
 
 // Routes
-router.post('/add', requiredLogin(), visitorController.createVisitor);                 // Any logged-in user (admin, guard)
+router.post('/add', requiredLogin('admin'), visitorController.createVisitor);                 // Any logged-in user (admin, guard)
 router.get('/:societyId', requiredLogin(), visitorController.getVisitors);             // Any logged-in user
 router.put('/status/:id', requiredLogin('resident'), visitorController.updateVisitorStatus); // Only residents can approve/reject
 router.put('/exit/:id', requiredLogin('admin'), visitorController.markVisitorExit);    // Only admin can mark exit
